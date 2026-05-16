81d3581cf68845b3fbbf080f775f2903ced7f4b4
hi03s <hi03sub@gmail.com>
2026-05-16 18:49:59 +0900

    不要なデバッグログの削除

--- src/assets/minecraft/scripts/lib_hi03toolkit_1_0/lib_UndoManager.ts
+++ src/assets/minecraft/scripts/lib_hi03toolkit_1_0/lib_UndoManager.ts
@@ -9,7 +9,6 @@ export class UndoManager {
     static backupFromBlockBuilder(entity: Entity, builder: BlockBuilder): void {
         const backupBuilder = new BlockBuilder();
         const posList = builder.get(entity);
-        NGTLog.debug(`[NGTO Builder2]Undo block count: ${posList.length}`);
         for (let i = posList.length - 1; i >= 0; i--) {
             const pos = posList[i];
             backupBuilder.addBackup(entity, [pos[1], pos[2], pos[3]]);
